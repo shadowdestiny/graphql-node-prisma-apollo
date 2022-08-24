7fa745c01d6313e18b2e7e346184c8e1629a4505
Luis <lrm.programador@gmail.com>
2022-08-24 09:40:41 -0400

    Add Mock test

--- src/index.js
+++ src/index.js
@@ -1,20 +1,37 @@
 const {ApolloServer, gql} = require('apollo-server')
+const {loadTypedefsSync} = require('@graphql-tools/load')
+const {GraphQLFileLoader} = require('@graphql-tools/graphql-file-loader')
+const {join} = require("path");
+
+const sources = loadTypedefsSync(join(__dirname, './types/typeDefs.gql'), {
+    loaders: [
+        new GraphQLFileLoader()
+    ]
+})
+
+const typeDefs = sources.map(source => source.document)
 
 console.log(`hola curso ${process.env.NODE_PORT}`);
 
-const typeDefs = gql `
+/*const typeDefs = gql `
     type Query {
         hello: String
     }
-`
-
+`*/
 const resolvers = {
+    Query: {
+        listarAeropuertos: () => {
+            return [{id: "hola", localizacion: "mundo", rutas: [], aviones: []}]
+        }
+    }
+}
+/*const resolvers = {
     Query: {
         hello: () => {
             return `hola curso ${process.env.NODE_PORT}`
         }
     }
-}
+}*/
 
 const server = new ApolloServer({
     typeDefs,
